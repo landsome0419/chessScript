c67563612a4180c18ee78ef38274e95050449170
landsome0419 <landsome@protonmail.com>
2019-09-21 18:03:25 -0700

    Added function for evaluating tags before making pgn file

--- chessPgnMaker.py
+++ chessPgnMaker.py
@@ -2,30 +2,58 @@
 import time
 import os
 
-event = raw_input("Event: ")
-date = raw_input("(yyyy.mm.dd) Date: " )
-roundd = raw_input("Round: ")
-white = raw_input("White: ")
-black = raw_input("Black: ")
-result = raw_input("(Ex: 1-0, 1/2 if draw) Result : ")
-white_elo = raw_input("(\"-\" for blank) White Elo: ")
-black_elo = raw_input("(\"-\" for blank) Black Elo: ")
+os.system('clear')
+
+event = input("Event: ")
+date = input("(yyyy.mm.dd) Date: " )
+roundd = input("Round: ")
+white = input("White: ")
+black = input("Black: ")
+result = input("(Ex: 1-0, 1/2 if draw) Result : ")
+white_elo = (input("(\"enter\" for blank) White Elo: ") or "-")
+black_elo = (input("(\"enter\" for blank) Black Elo: ") or "-")
 
 os.system('clear') 
 time.sleep(1)
 
-print("[Event ""\""+event+"\"]")
-time.sleep(.3)
-print("[Date ""\""+date+"]\"")
-time.sleep(.3)
-print("[Round ""\""+roundd+"\"]")
-time.sleep(.3)
-print("[White ""\""+white+"\"]")
-time.sleep(.3)
-print("[Black ""\""+black+"\"]")
-time.sleep(.3)
-print("[Result ""\""+result+"\"]")
-time.sleep(.3)
-print("[White Elo ""\""+white_elo+"\"]")
-time.sleep(.3)
-print("[Black Elo ""\""+black_elo+"\"]")
+list = [ event, date, roundd, black, result, white_elo, black_elo]
+
+def user_input(event, date, roundd, white, black, result, white_elo, black_elo):
+	print("(1) [Event ""\""+event+"\"]")
+	print("(2) [Date ""\""+date+"]\"")
+	print("(3) [Round ""\""+roundd+"\"]")
+	print("(4) [White ""\""+white+"\"]")
+	print("(5) [Black ""\""+black+"\"]")
+	print("(6) [Result ""\""+result+"\"]")
+	print("(7) [White Elo ""\""+white_elo+"\"]")
+	print("(8) [Black Elo ""\""+black_elo+"\"]")
+	return 
+
+
+
+
+
+
+x = 1
+while(x == 1):
+	user_input(event, date, roundd, white, black, result, white_elo, black_elo)
+	print ("\n")
+	answer = (input("Is everything above correct? (yes/no): ") or "yes")
+	if answer.casefold() in ['y', 'yes']:
+		x +=1
+
+	elif answer.casefold() in ['n', 'no']:
+		print ('lemme fix that')
+
+	else:
+		print ('Please enter a valid answer')
+		time.sleep (2)
+		os.system('clear')
+
+print ('out of loop')
+
+	
+
+
+
+
